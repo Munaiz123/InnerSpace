8aca7e8adeb97d45f44485e3cd29979472e879c4
Munaiz <munaizahmed2@gmail.com>
2020-03-17 14:11:10 -0500

    coding USER API - POST  & its test spec

--- server/api/users.js
+++ server/api/users.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
+// /api/users/
 router.get('/', async (req, res, next) => {
   try {
     let allUsers = await User.findAll({
@@ -10,5 +11,34 @@ router.get('/', async (req, res, next) => {
     res.send(allUsers).status(200)
   } catch (err) {
     next(err)
+  }
+})
+
+
+// /api/users/id
+router.get('/:id', async (req, res, next)=>{
+  try{
+    let user = await User.findOne({where:{id: req.params.id}})
+    console.log(req.params)
+    res.send(user).status(200)
+  } catch (error){
+    next (error)
+  }
+})
+
+// /api/users/addUser
+router.post('/addUser', async( req, res, next)=>{
+  try{
+    let newUser = await User.create({
+      email:req.body.email,
+      password:req.body.password,
+      firstName:req.body.firstName,
+      lastName:req.body.lastName,
+      isLandlord:req.body.isLandlord,
+    })
+    res.json(newUser).status(201)
+
+  } catch(error){
+    next(error)
   }
 })
--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -1,38 +1,83 @@
 /* global describe beforeEach it */
 
 const {expect} = require('chai')
+const sinon = require('sinon')
 const request = require('supertest')
 const db = require('../db')
 const app = require('../index')
+const agent = request.agent(app)
 const User = db.model('user')
 
+
 describe('User API routes', () => {
-  beforeEach(() => {
-    return db.sync({force: true})
-  })
+  let users = [
+    {id:1, email: 'user1@email.com',firstName:'User', lastName:'One'},
+    {id:2,email: 'user2@email.com',firstName:'User', lastName:'Two'},
+    {id:3,email: 'user3@email.com',firstName:'User', lastName:'Three'}
+  ]
 
-  describe('/api/users/', () => {
-    const codysEmail = 'cody@puppybook.com'
-    const firstName = 'cody'
-    const lastName = 'boi'
+  describe('GET /api/users/', () => {
 
+    if(!User.findAll) User.findAll = ()=>{}
+    let fakeFindAll = sinon.fake.resolves(users)
 
-    beforeEach( () => {
-      return User.create({
-        email: codysEmail,
-        firstName:firstName,
-        lastName:lastName
-      })
+    beforeEach(() => {
+      sinon.replace(User, 'findAll', fakeFindAll)
+      return db.sync({force: true})
+    })
+    afterEach(()=>{
+      sinon.restore()
     })
 
-    it('GET /api/users', async () => {
+    it('responds with all users in an array and should be called once', async () => {
       const res = await request(app)
         .get('/api/users')
+        .timeout({deadline:20})
         .expect(200)
 
       expect(res.body).to.be.an('array')
+      expect(res.body).to.deep.equal(users)
+      expect(User.findAll.calledOnce).to.be.equal(true)
+    })
 
+    // it('should be able to add a user to the data base', async ()=>{
+    //   const res = await request(app)
+    //     .post('/api/users/')
+    //     .timeout({deadline:20})
+    //     .expect(200)
+
+    // })
+  }) // end describe(' GET /api/users')
+
+
+  describe('POST /api/users', () => {
+
+    it('creates a new user and sends back the newly created user', async ()=>{
+      const res = await agent
+        .post('/api/users')
+        .send({
+          email: 'user4@email.com',
+          firstName: 'User',
+          lastName: 'Four'
+        })
+        .expect(201)
+
+      // expect(res.body).to.be.an('object')
+      // expect(res.body.lastName).to.equal('Four')
+
+      // const newUser = await User.findOne({
+      //   where:{
+      //     lastName:'Four'
+      //   }
+      // })
+
+      // expect(newUser).to.be.an('object')
+      // expect(newUser.lastName).to.equal('Four')
 
     })
-  }) // end describe('/api/users')
+
+  }) // end describe(' POST /api/users')
+
+
+
 }) // end describe('User routes')
