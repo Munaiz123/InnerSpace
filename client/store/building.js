b7b55f2fa28aa1bab04ddf5fec88767b13c72f6d
Munaiz <munaizahmed2@gmail.com>
2020-04-02 23:03:08 -0500

    added more thunks, action creators as well as upated reducer appropriately in store/building.js. Need to write tests and test with frontend. On to developing frontend next

--- client/store/building.js
+++ client/store/building.js
@@ -17,9 +17,9 @@ export const getSingleBuilding = singleBuilding =>({type: GET_BUILDING, singleBu
 
 export const addBuilding = building => ({type: ADD_BUILDING, building})
 
-export const updateBuilding = building =>({type: UPDATE_BUILDING, building})
+export const updateBuildingInfo = building =>({type: UPDATE_BUILDING, building})
 
-export const deleteBuilding = buidldingId =>({ type: DELETE_BUILDING, buidldingId})
+export const deleteBuildingId = buidldingId =>({ type: DELETE_BUILDING, buidldingId})
 
 //THUNKS
 
@@ -29,6 +29,49 @@ export const fetchBuildings = () => async dispatch =>{
     dispatch(getBuildings(res.data))
   }catch(error){
     console.log('ERROR FROM fetchBuildings THUNK ', error)
+  }
+}
+
+export const fetchSingleBuilding = () => async dispatch =>{
+  try{
+    let {data} = await axios.get(`/api/buildings/${id}`)
+    dispatch(getSingleBuilding(data))
+
+  } catch(error){
+    console.log('ERROR FROM fetchSingleBuildings THUNK ', error)
+  }
+}
+
+export const addBuild = building => async dispatch =>{
+  try{
+    let {data} = await axios.post(`/api/buildings/addBuilding`, {building})
+    dispatch(addBuilding(data))
+
+  } catch(error){
+    console.log('ERROR FROM addBuild THUNK ', error)
+  }
+}
+
+export const updateBuilding = (building, id) => async dispatch =>{
+  try{
+    let {data} = await axios.put(`/api/buildings/${id}`, {building})
+    dispatch(updateBuildingInfo(data))
+
+  } catch(error){
+    console.log('ERROR FROM updateBuilding THUNK ', error)
+  }
+}
+
+export const deleteBuilding = id => async dispatch =>{
+  try{
+    await axios.delete(`/api/buildings/${id}`)
+    console.log('ERROR FROM updateBuilding THUNK ', error)
+    dispatch(deleteBuildingId(buidldingId))
+
+  } catch(error){
+    console.log('ERROR FROM deleteBuilding THUNK ', error)
+
+
   }
 }
 
@@ -46,6 +89,16 @@ export default function(state = buildings, action){
   switch(action.type){
     case GET_BUILDINGS:
       return action.buildings
+    case GET_BUILDING:
+      return action.building
+    case ADD_BUILDING:
+      return action.building
+    case UPDATE_BUILDING:
+      return [...state].map(build =>{
+        if(build.id === action.building.id) return action.building
+      })
+    case DELETE_BUILDING:
+      return [...state].filter( build => action.id !== build.id)
     default:
       return state
   }
