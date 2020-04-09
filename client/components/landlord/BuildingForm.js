e901e31378925b91d5a31633e038352eb7fa674d
Munaiz <munaizahmed2@gmail.com>
2020-04-08 19:29:05 -0500

    added form, input fields + button to BuildingForm.js. Text is properly being assigned to state. Now need to pass info to store + back end

--- client/components/landlord/BuildingForm.js
+++ client/components/landlord/BuildingForm.js
@@ -6,17 +6,60 @@ import {Link} from 'react-router-dom'
 export class BuildingForm extends React.Component {
   constructor() {
     super()
+    this.state={}
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+
   }
 
-  componentDidMount() {
+  handleSubmit(){
+    event.preventDefault()
+    console.log(this.state)
+
+  }
+
+  handleChange(){
+    this.setState({[event.target.name]:event.target.value})
   }
 
   render() {
 
-
     return (
       <div>
-      <h3>FORM (TEST)</h3>
+        <form onSubmit={this.handleSubmit}
+        style={{display:'flex', flexDirection:'column', justifyContent:'spaceEvenly'}}
+        >
+
+        <input
+            type="text"
+            name="buildingName"
+            value={this.state.buildingName}
+            onChange={this.handleChange}
+            placeholder='Builing Name'
+          />
+
+
+          <input
+            type="text"
+            name="address"
+            value={this.state.address}
+            onChange={this.handleChange}
+            placeholder='Address'
+          />
+
+          <input
+            type="text"
+            name="unitsCount"
+            value={this.state.unitsCount}
+            onChange={this.handleChange}
+            placeholder='Number of Units'
+          />
+
+          <button type="submit">Add Building</button>
+        </form>
+
+
       </div>
     )
   }
