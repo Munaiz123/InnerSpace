ada4b971394eae6d96054a649417c627d20491c0
Munaiz <munaizahmed2@gmail.com>
2020-04-10 19:44:33 -0500

    may need to rethihnk model structure or find a way to laod in BuildingIDs and searching units based on those IDs

---
+++ server/api/units.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+const {Unit} = require('../db/models')
+const {isLandlord} = require('./middleware')
+
+router.get('/', async(req, res, next)=>{
+  try{
+    let allUnits = await Unit.findAll()
+    res.send(allUnits).status(200)
+
+  } catch(error){
+    next(error)
+  }
+})
