c6d97fb074d78d66cd50ab2f01bc5ef3e74ad4aa
Munaiz <munaizahmed2@gmail.com>
2020-04-20 16:04:41 -0500

    adding filter functionality to AllUnits.js

--- client/components/landlord/AllUnits.js
+++ client/components/landlord/AllUnits.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 import {fetchUnits} from '../../store/units'
+import {fetchBuildings} from '../../store/buildings'
 
 import SingleUnitCard from './SingleUnitCard'
 import AddUnitForm from './AddUnitForm'
@@ -9,27 +10,69 @@ import AddUnitForm from './AddUnitForm'
 export class AllUnits extends React.Component {
   constructor() {
     super()
+    this.state = {
+      search: ''
+    }
   }
 
   componentDidMount() {
     this.props.fetchUnits()
+    this.props.fetchBuildings()
   }
 
   render() {
-    let {allUnits} = this.props
+    let {allUnits, allBuildings} = this.props
+
+    console.log(allBuildings)
+
+
 
     return (
-      <div style={{display: 'flex', flexDirection: 'row', justifyContent:'spaceBetween'}}>
-        <div style={{width:'35%', backgroundColor:'yellow'}}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'spaceBetween'
+        }}
+      >
+        <div style={{width: '35%', backgroundColor: 'yellow'}}>
           <h1>My Units</h1>
           {allUnits.map((unit, i) => (
             <SingleUnitCard key={i} index={i + 1} unitInfo={unit} />
           ))}
         </div>
-
         <div>
-          <h3>ADD NEW UNIT</h3>
-          <AddUnitForm />
+          <div>
+            {/* <h4> SEARCH & FILTERS </h4> */}
+
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center'
+              }}
+            >
+              <h5 style={{paddingRight: '3px'}}> Filter by Building:</h5>
+
+              <select onChange={this.handleChange}>
+                {allBuildings.map((build,i) => <option key={i}>{build.buildingName}</option>)}
+              </select>
+
+              <input
+                name="search"
+                onChange={this.searchHandleChange}
+                defaultValue={this.state.search}
+                placeholder=" Type Here, bruv"
+              />
+            </div>
+          </div>
+
+          {/* end SEARCH & FILTERS */}
+
+          <div>
+            <h3>ADD NEW UNIT</h3>
+            <AddUnitForm />
+          </div>
         </div>
       </div>
     )
@@ -37,11 +80,13 @@ export class AllUnits extends React.Component {
 }
 
 const mapState = state => ({
-  allUnits: state.units
+  allUnits: state.units,
+  allBuildings: state.buildings
 })
 
 const mapDispatch = dispatch => ({
-  fetchUnits: () => dispatch(fetchUnits())
+  fetchUnits: () => dispatch(fetchUnits()),
+  fetchBuildings: () => dispatch(fetchBuildings())
 })
 
 export default connect(mapState, mapDispatch)(AllUnits)
