68bcf6f5f8b47470715fbf8839b9eb8b783adc58
Munaiz <munaizahmed2@gmail.com>
2020-05-05 18:49:17 -0500

    created TICKETS reducer + added it to apps store

--- client/store/index.js
+++ client/store/index.js
@@ -10,8 +10,9 @@ import units from './units'
 import unit from './singleUnit'
 import tenants from './tenants'
 import tenant from './singleTenant'
+import tickets from './tickets'
 
-export const reducer = combineReducers({user,buildings,building,units,unit,tenants,tenant})
+export const reducer = combineReducers({user,buildings,building,units,unit,tenants,tenant, tickets})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
---
+++ client/store/tickets.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+// ACTION TYPES
+
+const GET_TICKETS = 'GET_TICKETS'
+
+// ACTION CREATORS
+
+export const getTickets = tickets => {type: GET_TICKETS, tickets}
+
+//THUNKS
+
+export const fetchTickets = () => async dispatch =>{
+  try{
+    let {data} = await axios.get('/api/tickets')
+    dispatch(getTickets(data))
+
+  } catch(error){
+    console.log('ERROR FROM fetchTickets THUNK', error)
+  }
+}
+
+const tickets = []
+
+export default function(state = tickets, action){
+  switch(action.type){
+    case GET_TICKETS:
+      return action.tickets
+    default:
+      return state
+  }
+}
