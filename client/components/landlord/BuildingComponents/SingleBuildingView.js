01a6f9795fb60f341bba35d8f8a838f1aef0b837
Munaiz <munaizahmed2@gmail.com>
2020-05-28 22:07:10 -0500

    removed old code. Need to restyle/use material UI for SingleUnitCard.js

--- client/components/landlord/BuildingComponents/SingleBuildingView.js
+++ client/components/landlord/BuildingComponents/SingleBuildingView.js
@@ -27,66 +27,47 @@ export class SingleBuildingView extends React.Component {
 
 
     return (
-      <div>
-        <Grid>
-          <Grid container style={{padding:'2.5%'}}>
-            <Grid item xs={5}>
-              <div id="singleBuildInfo">
-                <h2>{building.buildingName}</h2>
-                <h5>Address: {building.address}</h5>
-                <h5>Number of Units: {building.unitsCount}</h5>
-              </div>
+      <Grid>
+        <Grid container style={{padding: '2.5%'}}>
+          <Grid item xs={5}>
+            <div id="singleBuildInfo">
+              <h2>{building.buildingName}</h2>
+              <h5>Address: {building.address}</h5>
+              <h5>Number of Units: {building.unitsCount}</h5>
+            </div>
           </Grid>
 
-          <Grid item xs={5} style={{display:'flex',flexDirection:'column', justifyContent:'center'}}>
+          <Grid
+            item
+            xs={5}
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center'
+            }}
+          >
             <EditBuildingForm buildId={building.id} />
           </Grid>
         </Grid>
-        <Grid container style={{padding:'2.5%'}}>
+        <Grid container style={{padding: '2.5%'}}>
           <Grid item xs={5}>
             <div>
               <h3>UNITS</h3>
-              {units.map((unit,i)=>(
-                <SingleUnitCard key={i} index={i +1} unitInfo={unit} tenInfo={unit.tenant}/>
+              {units.map((unit, i) => (
+                <SingleUnitCard
+                  key={i}
+                  index={i + 1}
+                  unitInfo={unit}
+                  tenInfo={unit.tenant}
+                />
               ))}
             </div>
           </Grid>
-          <Grid item lg={3} style={{backgroundColor:'cornSilk'}}>
+          <Grid item lg={3}>
             <AddUnitForm buildId={buildingId} />
           </Grid>
-        </ Grid>
-
+        </Grid>
       </Grid>
-
-       <div >
-        {/* <div style={{display: 'flex', flexDirection: 'row'}}>
-          <div id="singleBuildInfo" style={{width: '40%'}}>
-            <h4>{building.buildingName}</h4>
-            <h5>Address: {building.address}</h5>
-            <h5>Number of Units: {building.unitsCount}</h5>
-          </div>
-          <div style={{width: '40%'}}>
-            <h4>Edit Building</h4>
-            <EditBuildingForm buildId={building.id} />
-          </div>
-        </div> */}
-
-        <div style={{display: 'flex', flexDirection: 'row', paddingTop:'5%', alignItems:'baseline'}}>
-          {/* <div style={{width: '40%'}}>
-            <h3>UNITS</h3>
-
-            <div>
-              {units.map((unit,i)=>(
-                <SingleUnitCard key={i} index={i +1} unitInfo={unit} tenInfo={unit.tenant}/>
-              ))}
-            </div>
-          </div> */}
-          <div>
-            {/* <h3 style={{marginBottom: '5px', marginTop: '0px'}}>ADD NEW UNIT</h3> */}
-          </div>
-        </div>
-      </div>
-      </div>
     )
   }
 }
